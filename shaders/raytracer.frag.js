8e8ff5d28a812c89193988f9467fa82ce16fb1bc
bstempelj <blaz.stempelj@gmail.com>
2020-05-15 12:45:07 +0200

    added incomplete csg algorithm code

--- shaders/raytracer.frag.js
+++ shaders/raytracer.frag.js
@@ -1,12 +1,80 @@
 export default `#version 300 es
 precision highp float;
 
+////////////////////////////////////////////////////////////////////////////////
+// DEFINES
+// states
+#define NONE 0
+#define GOTOLFT 1
+#define GOTORGH 2
+#define LOADLFT 3
+#define LOADRGH 4
+#define SAVELFT 5
+#define CLASSIFY 6
+
+// actions
+#define RETLIFCLOSER 0
+#define RETRIFCLOSER 1
+#define RETL 3
+#define RETR 4
+#define LOOPL 5
+#define LOOPR 6
+#define LOOPLIFCLOSER 7
+#define LOOPRIFCLOSER 8
+#define FLIPNORMR 9
+
+// hit types
+#define ENTER 0
+#define EXIT 1
+#define MISS 2
+
+// node types
+#define OP 0
+#define LF 1
+
+// operations
+#define VIRTL 0
+#define UNION 1
+#define INTER 2
+#define SUBST 3
+
+// primitives
+#define SPHERE 0
+#define CUBE 1
+#define CYLINDER 2
+
+// stack
+#define STACK_SIZE 10
+
+////////////////////////////////////////////////////////////////////////////////
+// SHADER VARIABLES
 uniform vec2 u_res;
 uniform highp usampler2D u_csgtree;
 uniform sampler2D u_spheres;
 
 out vec4 o_fragColor;
 
+////////////////////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES
+int stateHead = -1;
+int hitHead   = -1;
+int stateStack[STACK_SIZE];
+int hitStack  [STACK_SIZE];
+
+////////////////////////////////////////////////////////////////////////////////
+// STRUCTS
+// struct ray {
+// 	vec3 origin;
+// 	vec3 direct;
+// };
+
+// struct hitpoint {
+// 	float t;
+// 	vec3 normal;
+// };
+
+////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
 vec3 nSphere(vec3 pos, vec4 sph) {
 	return (pos-sph.xyz) / sph.w;
 }
@@ -29,6 +97,279 @@ vec4 iSphere(vec3 ro, vec3 rd, int node) {
 	return vec4(t, nor);
 }
 
+// // stack push helpers
+// void pushState(int state)       { stateStack[++stateHead] = state;     }
+// void pushHit(float t, int node) { hitStack[++hitHead] = vec2(t, node); }
+// void pushTime(float t)          { hitStack[++hitHead] = vec2(t, -1.0); }
+
+// // stack pop helpers
+// int   popState()       { return stateStack[stateHead--]; }
+// vec4  popHit(int node) { return hitStack[hitHead--];     }
+// float popTime()        { return hitStack[hitHead--].x;   }
+
+// // node access helpers
+// int left  (int node) { return 2*node + 1;     }
+// int right (int node) { return 2*node + 2;     }
+// int parent(int node) { return (node - 1) / 2; }
+
+// // node type helpers
+// bool primitive(int node) { return tree[node].type == LF; }
+// bool operation(int node) { return tree[node].type == OP; }
+
+// int classifyHit(vec3 rd, vec4 isect) {
+//     if (isect.x < 0.0) return MISS;
+//     float res = dot(normalize(rd), isect.yzw);
+//     if (res > 0.0) return EXIT;
+//     else return ENTER;
+// }
+
+// bool hasAction(ivec3 actions, int action) {
+// 	return actions.x == action
+// 		|| actions.y == action
+// 		|| actions.z == action;
+// }
+
+// ivec3 stateTable(int op, int hitL, int hitR) {
+// 	ivec3 actions = ivec3(-1);
+
+// 	// UNION
+//     if (op == UNION) {
+//         // left ENTER
+//         if (hit_l == ENTER && hit_r == ENTER) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = RETRIFCLOSER;
+//         }
+//         else if (hit_l == ENTER && hit_r == EXIT) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = LOOPL;
+//         }
+//         else if (hit_l == ENTER && hit_r == MISS) {
+//             actions.x = RETL;
+//         }
+
+//         // left EXIT
+//         else if (hit_l == EXIT && hit_r == ENTER) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = LOOPR;
+//         }
+//         else if (hit_l == EXIT && hit_r == EXIT) {
+//             actions.x = LOOPLIFCLOSER;
+//             actions.y = LOOPRIFCLOSER;
+//         }
+//         else if (hit_l == EXIT && hit_r == MISS) {
+//             actions.x = RETL;
+//         }
+
+//         // left MISS
+//         else if (hit_l == MISS && (hit_r == ENTER || hit_r == EXIT)) {
+//             actions.x = RETR;
+//         }
+//         else if (hit_l == MISS && hit_r == MISS) {
+//             actions.x = MISS;
+//         }
+//     }
+//     // INTERSECTION
+//     else if (op == INTER) {
+//         // left ENTER
+//         if (hit_l == ENTER && hit_r == ENTER) {
+//             actions.x = LOOPLIFCLOSER;
+//             actions.y = LOOPRIFCLOSER;
+//         }
+//         else if (hit_l == ENTER && hit_r == EXIT) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = LOOPR;
+//         }
+//         else if (hit_l == ENTER && hit_r == MISS) {
+//             actions.x = MISS;
+//         }
+
+//         // left EXIT
+//         else if (hit_l == EXIT && hit_r == ENTER) {
+//             actions.x = RETRIFCLOSER;
+//             actions.y = LOOPL;
+//         }
+//         else if (hit_l == EXIT && hit_r == EXIT) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = RETRIFCLOSER;
+//         }
+//         else if (hit_l == EXIT && hit_r == MISS) {
+//             actions.x = MISS;
+//         }
+
+//         // left MISS
+//         else if (hit_l == MISS && (hit_r == ENTER || hit_r == EXIT || hit_r == MISS)) {
+//             actions.x = MISS;
+//         }
+//     }
+//     // SUBSTRACTION
+//     else if (op == SUBST) {
+//         // left ENTER
+//         if (hit_l == ENTER && hit_r == ENTER) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = LOOPR;
+//         }
+//         else if (hit_l == ENTER && hit_r == EXIT) {
+//             actions.x = LOOPLIFCLOSER;
+//             actions.y = LOOPRIFCLOSER;
+//         }
+//         else if (hit_l == ENTER && hit_r == MISS) {
+//             actions.x = RETL;
+//         }
+
+//         // left EXIT
+//         else if (hit_l == EXIT && hit_r == ENTER) {
+//             actions.x = RETLIFCLOSER;
+//             actions.y = RETRIFCLOSER;
+//             actions.z = FLIPNORMR;
+//         }
+//         else if (hit_l == EXIT && hit_r == EXIT) {
+//             actions.x = RETRIFCLOSER;
+//             actions.y = FLIPNORMR;
+//             actions.z = LOOPL;
+//         }
+//         else if (hit_l == EXIT && hit_r == MISS) {
+//             actions.x = RETL;
+//         }
+
+//         // left MISS
+//         else if (hit_l == MISS && (hit_r == ENTER || hit_r == EXIT || hit_r == MISS)) {
+//             actions.x = MISS;
+//         }
+//     }
+
+//     return actions;
+// }
+
+// ivec2 sceneNearestHit(ro, rd) {
+// 	vec2 result = ivec2(-1);
+
+// 	float tstart = 0.0;
+// 	int node = 0; // node to traverse (root == 0)
+// 	int state = GOTOLFT;
+
+// 	vec4 isectL, isectR;
+// 	bool traverseL, traverseR;
+
+// 	pushState(CLASSIFY);
+
+// 	for (bool toContinue = true; toContinue; ) {
+// 		if (state == SAVELFT) {
+// 			pushHit(isectL.x, node);
+// 			tstart = popTime();
+// 			state = GOTORGH;
+// 		}
+// 		if (state == GOTOLFT || state == GOTORGH) {
+// 			if (state == GOTOLFT) {
+// 				node = left(node);
+// 			} else {
+// 				node = right(node);
+// 			}
+
+// 			if (operation(node)) {
+// 				// AABB was hit (not implemented yet, just assume it was)
+// 				traverseL = true;
+// 				traverseR = true;
+
+// 				if (traverseL && primitive(left(node))) {
+// 					isectL = iSphere(left(node), tstart);
+// 					traverseL = false;
+// 				}
+// 				if (traverseR && primitive(right(node))) {
+// 					isectR = iSphere(right(node), tstart);
+// 					traverseR = false;
+// 				}
+
+// 				if (traverseL || traverseR) {
+// 					if (!traverseL) {
+// 						pushHit(isectL.x, left(node));
+// 						pushState(LOADLFT);
+// 					}
+// 					else if (!traverseR) {
+// 						pushHit(isectR.x, right(node));
+// 						pushState(LOADRGH);
+// 					}
+// 					else {
+// 						pushTime(tstart);
+// 						pushState(LOADLFT);
+// 						pushState(SAVELFT);
+// 					}
+
+// 					if (traverseL) {
+// 						state = GOTOLFT;
+// 					} else {
+// 						state = GOTORGH;
+// 					}
+// 				}
+// 				else {
+// 					state = CLASSIFY;
+// 				}
+// 			}
+// 			else { // primitive(node)
+// 				if (state == GOTOLFT) {
+// 					isectL = iSphere(node, tstart);
+// 				} else {
+// 					isectR = iSphere(node, tstart);
+// 				}
+// 				state = CLASSIFY;
+// 				node = parent(node); // go to parent
+// 			}
+// 		}
+// 		if (state == LOADLFT || state == LOADRGH || state == CLASSIFY) {
+// 			if (state == LOADLFT || state == LOADRGH) {
+// 				if (state == LOADLFT) {
+// 					isectL = popHit(node);
+// 				} else {
+// 					isectR = popHit(node);
+// 				}
+// 			}
+
+// 			int hitL = classifyHit(rd, isectL);
+// 			int hitR = classifyHit(rd, isectR);
+// 			int op   = texelFetch(u_csgtree, ivec2(node, 0), 0).y;
+
+// 			ivec3 actions = stateTable(op, hitL, hitR);
+// 			if (hasAction(actions, RETL)
+// 			|| (hasAction(actions, RETLIFCLOSER) && isectL.x <= isectR.x)) {
+// 				isectR = isectL;
+// 				result = isectL;
+// 				toContinue = (aHead >= 0);
+// 				state = popState();
+// 				node = parent(node);
+// 			}
+// 			else if (hasAction(actions, RETR)
+// 				 || (hasAction(actions, RETRIFCLOSER) && isectR.x < isectL.x)) {
+// 				isectL = isectR;
+// 				result = isectR;
+// 				toContinue = (stateHead >= 0);
+// 				state = popState();
+// 				node = parent(node);
+// 			}
+// 			else if (hasAction(actions, LOOPL)
+// 				 || (hasAction(actions, LOOPLIFCLOSER) && isectL.x <= isectR.x)) {
+// 				tstart = isectL.x;
+// 				pushHit(isectR.x, node);
+// 				pushState(LOADRGH);
+// 				state = GOTOLFT;
+// 			}
+// 			else if (hasAction(actions, LOOPR)
+// 				 || (hasAction(actions, LOOPRIFCLOSER) && isectR.x < isectL.x)) {
+// 				tstart = isectR.x;
+// 				pushHit(isectL.x, node);
+// 				pushState(LOADLFT);
+// 				state = GOTORGH;
+// 			}
+// 			else {
+// 				isectR.x = -1.0;
+// 				toContinue = (stateHead >= 0);
+// 				result = vec3(-1);
+// 				state = popState();
+// 			}
+// 		}
+// 	}
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+// MAIN
 void main() {
 	vec3 light = normalize(vec3(0.57703));
 	// uv are pixel coordinates, from 0 to 1
