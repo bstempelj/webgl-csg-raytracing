d1e6f25c68f23c472f5ce9c79b202439cb0295f8
bstempelj <blaz.stempelj@gmail.com>
2020-06-05 12:41:47 +0200

    added box and cylinder intersection functions (from inigo quilez)

--- shaders/raytracer.frag.js
+++ shaders/raytracer.frag.js
@@ -108,6 +108,52 @@ vec4 iSphere(vec3 ro, vec3 rd, int node, float tmin, float tmax) {
 	return INVALID_HIT;
 }
 
+// axis aligned box centered at the origin, with size boxSize
+vec2 iBox(vec3 ro, vec3 rd, vec3 boxSize, out vec3 outNormal) {
+    vec3 m = 1.0/rd; // can precompute if traversing a set of aligned boxes
+    vec3 n = m*ro;   // can precompute if traversing a set of aligned boxes
+    vec3 k = abs(m)*boxSize;
+    vec3 t1 = -n - k;
+    vec3 t2 = -n + k;
+    float tN = max( max( t1.x, t1.y ), t1.z );
+    float tF = min( min( t2.x, t2.y ), t2.z );
+    if(tN > tF || tF < 0.0) return INVALID_HIT;
+    outNormal = -sign(rdd)*step(t1.yzx,t1.xyz)*step(t1.zxy,t1.xyz);
+    return vec2(tN, tF);
+}
+
+// extreme a, extreme b, radius
+vec4 iCylinder(vec3 ro, vec3 rd, vec3 pa, vec3 pb, float ra) {
+    vec3 ba = pb-pa;
+    vec3  oc = ro - pa;
+
+    float baba = dot(ba,ba);
+    float bard = dot(ba,rd);
+    float baoc = dot(ba,oc);
+    
+    float k2 = baba            - bard*bard;
+    float k1 = baba*dot(oc,rd) - baoc*bard;
+    float k0 = baba*dot(oc,oc) - baoc*baoc - ra*ra*baba;
+    
+    float h = k1*k1 - k2*k0;
+    if( h<0.0 ) return vec4(-1.0);
+    h = sqrt(h);
+    float t = (-k1-h)/k2;
+
+    // body
+    float y = baoc + t*bard;
+    if( y>0.0 && y<baba ) return vec4( t, (oc+t*rd - ba*y/baba)/ra );
+    
+    // caps
+    t = ( ((y<0.0) ? 0.0 : baba) - baoc)/bard;
+    if( abs(k1+k2*t)<h )
+    {
+        return vec4( t, ba*sign(y)/baba );
+    }
+
+    return INVALID_HIT;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
 // stack push helpers
